4fa364f25fe744741eb7e15e72a65531aa23ae59
Owain Llewellyn <owain.llew@gmail.com>
2012-08-21 17:15:05 +0100

    BaseFiles - A simple base template structure

---
+++ js/base-script.js
@@ -0,0 +1,9 @@
+/* Base Javascript File
+ * Author: Full Name
+ * Project Start Date: 00/00/00
+ ******************************************/
+
+ (function(window, document, $ undefined){
+ 	'use strict';
+
+ })(window, document, jQuery);
